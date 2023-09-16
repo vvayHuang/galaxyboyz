b44a85d686493a3b0bef2f6f1cd28b1b97503d86
wayHuang <88394654+vvayHuang@users.noreply.github.com>
2023-09-16 10:35:36 +0800

    refactor: 💡 pages,js,img

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,88 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 3,
-  spaceBetween: 24,
-  scrollbar: {
-    el: '.swiper-scrollbar',
+const theLatestSwiper = new Swiper('.the-latest-swiper', {
+  breakpoints: {
+    425: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  mousewheel: true,
+});
+const cdSwiper = new Swiper('.cd-swiper', {
+  breakpoints: {
+    425: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 18
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  mousewheel: true,
+});
+const vinylSwiper = new Swiper('.vinyl-swiper', {
+  breakpoints: {
+    slidesPerView: 1,
+    425: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 18
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  mousewheel: true,
+});
+const clothingSwiper = new Swiper('.clothing-swiper', {
+  breakpoints: {
+    slidesPerView: 1,
+    425: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 18
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  mousewheel: true,
+});
+const faqSwiper = new Swiper('.faq-swiper', {
+  breakpoints: {
+    slidesPerView: 1,
+    425: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 18
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   mousewheel: true,
 });
